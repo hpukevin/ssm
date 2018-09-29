93abc70b668addd24f3148428d7a5a2a6be0e7c3
kevin <kevin@kevin-PC>
2018-09-30 02:21:36 +0800

    allssm

---
+++ e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,35 @@
+package cn.e3mall.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
+import cn.e3mall.pojo.TbItemExample.Criteria;
+import cn.e3mall.service.ItemService;
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+	private TbItemMapper  itemMapper;
+	@Override
+	public TbItem getItemById(long id) {
+		//TbItem item = itemMapper.selectByPrimaryKey(id);
+		TbItemExample tbItemExample=new TbItemExample();
+		Criteria criteria = tbItemExample.createCriteria();
+		criteria.andIdEqualTo(id);
+		List<TbItem> list = itemMapper.selectByExample(tbItemExample);
+		if (list!=null && list.size()>0) {
+			return list.get(0);
+		}
+		else {
+			return null;
+		}
+		
+		
+		
+	}
+
+}
